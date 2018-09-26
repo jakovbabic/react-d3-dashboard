73defb1eca3768a4a2890ab99f0ad601fc2b88e0
jakovbabic <jakovbabic.upwork@gmail.com>
2018-09-26 17:50:00 -0600

    update(dashboardview): updated graphs on dashboard view page

--- src/routes/DashboardView/components/index.js
+++ src/routes/DashboardView/components/index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import IconButton from '@material-ui/core/IconButton';
-import CardMedia from '@material-ui/core/CardMedia';
 import Settings from '@material-ui/icons/Settings';
 import './styles.css';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { BarChart, PieChart } from 'react-d3-components';
 import MoreHoriz from '@material-ui/icons/MoreHoriz';
 import Navbar from '../../../components/Navbar/container/index';
 
@@ -53,6 +52,96 @@ class DashboardView extends Component {
    * @param {Boolean} loaded
    */
 
+  makeTemplate = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  tooltip = (x, y, z) => {
+    if(z){
+      return `x: ${x}, y: ${z}`;
+    }
+    return `x: ${x}, y: ${y}`;
+  };
+
+  makeTemplate = (item) => {
+    if(item.type === 1){
+      const title = item.eeff[0].values[0].calcValue;
+      const subtitle = item.name;
+      const img = <img src={require('assets/images/logo.png')} alt="test" width='50px' height='50px' />;
+      return (
+        <div>
+          <h3>{ title }</h3>
+          <span>{ subtitle }</span>
+          { img }
+        </div>
+      );
+    }
+    if(item.type === 2){
+      const title = item.name;
+      const gdata = item.epigraph.map((p, i) => {
+        return {
+          x: p.name,
+          y: item.eeff[0].values[i].calcValue,
+        };
+      });
+      const data = {
+        label: 'somethingA',
+        values: gdata,
+      };
+      return (
+        <div className='text-align-center'>
+          <h5>{ title }</h5>
+          <PieChart
+            data={data}
+            width={400}
+            height={400}
+            margin={
+              {
+                top: 10, bottom: 10, left: 100, right: 100,
+              }
+            }
+            tooltipHtml={this.tooltip}
+          />
+        </div>
+      );
+    }
+    if(item.type === 3){
+      const title = item.name;
+      const grdata = item.eeff.map((p) => {
+        return {
+          x: p.name,
+          y: p.values[0].calcValue,
+        };
+      });
+      const data = [{
+        values: grdata,
+      }];
+      console.log(data);
+      return (
+        <div className='text-align-center'>
+          <h5>{ title }</h5>
+          <BarChart
+            data={data}
+            width={400}
+            height={400}
+            yAxis={
+              {
+                label: item.epigraph[0].name,
+              }
+            }
+            margin={
+              {
+                top: 10, bottom: 30, left: 100, right: 0,
+              }
+            }
+            tooltipHtml={this.tooltip}
+          />
+        </div>
+      );
+    }
+    return true;
+  };
+
   render() {
     const { dashboard, literals } = this.props;
     const { anchorEl } = this.state;
@@ -84,29 +173,24 @@ class DashboardView extends Component {
             </div>
           </div>
           <div className='DashboardView--graph'>
-            { dashboard.graph && dashboard.graph.map((item, i) => {
-              let title = '';
-              let subtitle = '';
-              let img = '';
-              if (item.type === 1) {
-                title = item.eeff[0].values[0].calcValue;
-                subtitle = item.name;
-                img = <CardMedia image={item.image} title="Live from space album cover" />;
+            <Grid container spacing={24}>
+              { dashboard.graph && dashboard.graph.map((item, i) => {
+                let width = 3;
+                if (item.type !== 1) {
+                  width = 4;
+                }
+                const template = this.makeTemplate(item);
+                return (
+                  <Grid item xs={width} key={i}>
+                    <Paper className='DashboardView--graph__item'>
+                      <IconButton className='DashboardView--graph__item--setting'><Settings /></IconButton>
+                      {template}
+                    </Paper>
+                  </Grid>
+                );
+              })
               }
-              return (
-                <Card key={i}>
-                  <CardHeader
-                    action={<IconButton><Settings /></IconButton>}
-                    title={title}
-                    subheader={subtitle}
-                  />
-                  <CardContent>
-                    { img }
-                  </CardContent>
-                </Card>
-              );
-            })
-            }
+            </Grid>
           </div>
         </div>
       </div>
