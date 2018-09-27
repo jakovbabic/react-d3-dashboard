90fd34d2d6775ed201a84a059c29f320bfcbbf09
Jesus Dominguez <jesus.dominguez@globalincubator.com>
2018-09-27 11:28:59 +0200

    chore(base): update base, add assets for indicators

--- src/constants/apiConf.js
+++ src/constants/apiConf.js
@@ -1,8 +1,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_URL;
-export const API_TOKEN = 'TTTTTTTTT';
-export const API_SECRET = 'SSSSSSSSSS';
 
 export const HEADERS = {
-  Token: API_TOKEN,
-  Secret: API_SECRET,
+  Accept: 'application/json',
 };
